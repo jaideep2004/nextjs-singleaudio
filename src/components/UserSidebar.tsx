ed6ddce70bdd394d33552190c19545a95641ffb5
Jaideep Singh <jaisidhu2004@gmail.com>
2026-06-18 18:01:57 +0530

    18 june 2026 3rd

--- src/components/UserSidebar.tsx
+++ src/components/UserSidebar.tsx
@@ -208,7 +208,7 @@ export default function UserSidebar() {
             '&:hover': { bgcolor: isDark ? 'rgba(255,255,255,0.06)' : 'rgba(0,0,0,0.04)' },
           }}
         >
-          <ChevronLeftIcon />
+          <ChevronLeftIcon />  
         </IconButton>
         <Tooltip title={desktopCollapsed ? 'Expand Sidebar' : 'Collapse Sidebar'} placement="right">
           <IconButton
@@ -396,7 +396,7 @@ export default function UserSidebar() {
             letterSpacing: '0.02em',
           }}
         >
-          Single Audio v2.0
+          Single Audio
         </Typography>
       </Box>
     </Box>
